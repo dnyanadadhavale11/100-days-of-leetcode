c1f6d841167344f660e648a88f1d391281417099
Dnyanada Dhavale <dnyanadadhavale11@gmail.com>
2026-07-08 18:58:09 +0530

    Day 5: Solved LeetCode 383 - Ransom Note

---
+++ HashMap/0383_Ransom_Note.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+
+        count = {}
+
+        for ch in magazine:
+            count[ch] = count.get(ch, 0) + 1
+
+        for ch in ransomNote:
+            if count.get(ch, 0) == 0:
+                return False
+            count[ch] -= 1
+
+        return True     
