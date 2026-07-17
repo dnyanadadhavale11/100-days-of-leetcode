b54cf5f68475ccb362088ca01a0da4b076ab23a1
Dnyanada Dhavale <dnyanadadhavale11@gmail.com>
2026-07-17 21:30:46 +0530

    Day 14: Solve LeetCode #704 - Binary Search

--- BinarySearch/0704_Binary_Search.py
+++ BinarySearch/0704_Binary_Search.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def searchInsert(self, nums, target):
+    def search(self, nums, target):
         left = 0
         right = len(nums) - 1
 
@@ -13,4 +13,4 @@ class Solution(object):
             else:
                 right = mid - 1
 
-        return left
+        return -1
